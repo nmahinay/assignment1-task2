aa5efe42487f4ae8c64a823a3dada8f5992d0624
Nicolai <nmahinay@gmail.com>
2020-08-21 10:19:15 +0800

    Done on adding comments

--- services/app.js
+++ services/app.js
@@ -2,12 +2,15 @@ const MongoClient = require('mongodb').MongoClient;
 var express=require('express')
 var moment=require('moment')
 var bodyParser=require('body-parser')
-var multer=require('multer')
+var multer=require('multer');
+const { ObjectID, ObjectId } = require('mongodb');
 
 var app=express()
+var username="Guest"
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // SET STORAGE
 var storage = multer.diskStorage({
@@ -16,7 +19,7 @@ var storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now()+'.jpg')
-    }
+    },
     
   })
   var upload = multer({ storage: storage })
@@ -27,50 +30,62 @@ var log=function(message){
     console.log('[Server] @'+time+' '+message)
 }
 
-//photos endpoint
+//get photos endpoint
 app.get('/photos',(req,res) => {
     retrievePhotos(res)
 })
 
-/*
-app.post('/photo', upload.single('photo'), (req, res, next) => {
-    const file = req.file
-    if (!file) {
-      const error = new Error('Please upload a file')
-      error.httpStatusCode = 400
-      return next(error)
-    }
-
-    insertPhoto(file)
-      res.send(file)
-  })
-*/
-
-//photo upload endpoint
-app.post('/photo', upload.single('photo'), (req, res, next) => {
+//post photos endpoint
+app.post('/photos', upload.single('photo'), (req, res, next) => {
   const file = req.file
+  var commentval=[]
+  file["username"]=username
+  file["comment"]=commentval
   if (!file) {
     const error = new Error('Please upload a real file')
     error.httpStatusCode = 400
     return next(error)
   }
-
   insertPhoto(file)
     res.redirect('/')
 })
+
+//post comment endpoint
+app.post('/comments', (req, res)=>{
+  insertComment(req,res)
+})
+
+//get comment endpoint
+app.get('/comments', (req, res)=>{
+  retrieveComments(res)
+})
+
+//
+
 
 var port=3000;
 app.listen(port)
 log('Server listening on: '+port)
 
+//mongodb connection
 const uri = "mongodb+srv://sit725:sit725@assignment1.vpqu2.mongodb.net/instagram?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
+//collection objects
 let collectionPhotos;
 
+
+client.connect(err => {
+ collectionPhotos = client.db("instagram").collection("photos")
+});
+
+//collection photos functions
+
 const insertPhoto=(file) =>{
+  //console.log(file)
   collectionPhotos.insertOne(file)
 }
+
 
 const retrievePhotos=(res)=>{
   collectionPhotos.find().toArray(function(err,result){
@@ -81,7 +96,44 @@ const retrievePhotos=(res)=>{
   })
 }
 
-client.connect(err => {
- collectionPhotos = client.db("instagram").collection("photos");
-});
+//collection comments functions
+const insertComment=(req,res)=>{
+  let message=req.body.message
+  let photoid=req.body.photoid
+  let username=req.body.username
+ 
+
+  collectionPhotos.update(
+    {  "_id":new ObjectId(photoid) },
+    {
+      $push: {
+        comment: {
+                'message': message,
+                'username': username
+              }
+      }
+  })
+   
+  res.send({
+    message, username, photoid
+  });
+}
+
+const retrieveComments=(res)=>{
+  collectionComments.find().toArray(function(err,result){
+
+    if (err) throw err;
+    res.send(result)
+  })
+}
+
+//login
+app.get('/login', (req,res)=>{
+  console.log('login', req.body)
+})
+
+app.post('/login', (req,res)=>{
+  let data = req.body;
+  username = data.name;
+})
 
--- services/public/env.js
+++ services/public/env.js
@@ -1,51 +1,108 @@
 $(document).ready(function(){
-    $(".dropdown-trigger").dropdown();
-/*
-    $('#btnUpPhoto').click(()=>{
-      let photo=$('#photo').val()
-      let data={
-        photo
-      }
-    $.get('/photo',data,function(){
+    $(".dropdown-trigger").dropdown()
 
-})
+    //initialize modal window
+    $('.modal').modal();
+
+    let username='Guest';
+    
+    $('#login').click(()=>{
+      let user=$('#nameBox').val()
+      
+      if (user.length <= 0) {
+        user = 'Guest'
+        username = 'Guest'
+        console.log('guest')
+      } else {
+        username = user
+        console.log('Welcome '+username)
+      }
+      $('#user').html(username)
+      let data={
+        name:user
+      }
+
+      $.ajax({
+        url:'/login',
+        contentType:'application/json',
+        data:JSON.stringify(data),
+        type:'POST',
+        success:function(result){
+          console.log(result)
+        }
+      })
+      alert('Welcome ' + user + '!')
 
     })
-    */
-   
-      $.get('/photos', function(file){
-        $('#photos').empty()
-        file.reverse()
-        file.forEach((image)=>{
-          //console.log(image.path)
-          var pattern = "photos/"
-          var imagePath = image.path
-          var path=imagePath.substr(imagePath.indexOf(pattern), imagePath.length)
-          var appendstr = '<div class="row">'
-          +'<img src="'+path+'" width="480" height="360">'
-          +'<a class="waves-effect waves-light btn">Like</a>'
-          +'<div class="input-field col s12">'
-          +'<input id="messageBox" type="text" class="validate">'
-          +'<label class="active" for="messageBox">Message</label>'
-          +'<a id="btnMessage" class="waves-effect waves-light btn">Add a comment</a>'
-          +'</div>'
-          +'</div>'
-          //console.log(appendstr)
-          $('#photos').append(appendstr)
-      
-        })
-      })
-  
 
-      /*<div class="input-field col s12">
-            <input id="messageBox" type="text" class="validate">
-            <label class="active" for="messageBox">Message</label>
-        </div>
-        </div>
-        <!--<a id="btnMessage" class="waves-effect waves-light btn">Post a message</a>-->
-        <a id="btnMessage" class="waves-effect waves-light btn">Post a message</a>*/
+    $.get('/photos', function(file){
+      $('#photos').empty()
+      file.reverse()
+      file.forEach((image)=>{
+        var commentsTag = '';
+        image.comment.forEach((com) =>{
+          commentsTag += com.username + ': '+ com.message + '<br>'
+       })
+
+
+        var pattern = "photos/"
+        var photoid = image._id
+        console.log(photoid)
+        var imagePath = image.path
+        var path=imagePath.substr(imagePath.indexOf(pattern), imagePath.length)
+        var appendstr = '<div class="row">'
+        +'<h6 class="username">'+image.username+'</h6>'
+        +'<img class="photoPost" src="'+path+'" width="480" height="360" id="'+ photoid +'">'
+        +'<div class="likeCommentbtn">'
+        +'<a class="waves-effect waves-light btn">Like</a>'
+        +'<a id="btnCommentModal" href="#addCommentModal" data-photo-id="'+photoid+'" class="waves-effect waves-light btn modal-trigger">Add a comment</a>'
+        +'</div>'
+        +'<div class="Comment" id="comments" data-comment-id="'+photoid+'">'+commentsTag+'</div>'
+        +'</div>'
+        $('#photos').append(appendstr)
         
-        $('.modal').modal();
+      })
+    })
+
+    $('#btnComment').click((e)=>{
+      let message=$('#messageBox').val()
+      if (username){
+        let photoid = $(e.target).data('photo-id')
+      let data={
+          username,
+          message,
+          photoid
+      }
+
+      $.ajax({
+        url:'/comments',
+        contentType:'application/json',
+        data:JSON.stringify(data),
+        type:'POST',
+        success:function(result){
+          console.log(result)
+          $('#addCommentModal').modal('close');
+          $('#messageBox').val('');
+          $("div[data-comment-id='"+ photoid +"']").append(result.username + ': '+ result.message + '<br>')
+
+        }
+      })
+      } else {
+        alert('Please log in')
+      }
+      
+    })
+
+    $.get('/comments', function(messages){
+      $('#messages').empty()
+      messages.forEach((message)=>{
+        $('#messages').append('<div class="row">'+message.message+'</div>')
+        
+      })
+    })
 
 })
 
+$(document).on('click', '#btnCommentModal',(e) => {
+  $('#btnComment').attr('data-photo-id',$(e.target).data('photo-id'))
+})
